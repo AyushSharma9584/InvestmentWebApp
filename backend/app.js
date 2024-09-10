df4ab410bbc2f9093a5679f92e7a12d0d94cea3f
Ayush sharma <ayushsharma83499@gmail.com>
2024-09-10 21:29:55 +0530

    changes  in env

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors")
 const app = express();
 const AuthRouter = require('./Router/Auth.router');
-const ProductRouter = require('./Router/Product.router')
+// const ProductRouter = require('./Router/Product.router')
 
 app.use(express.json())
 app.use(cors())
